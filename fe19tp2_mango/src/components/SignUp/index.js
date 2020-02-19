8b9c7d767e999f965fa69e586f589ba5b3c9c864
TomEder <tomederstal@gmail.com>
2020-02-19 11:36:23 +0100

    Realtime DB

--- fe19tp2_mango/src/components/SignUp/index.js
+++ fe19tp2_mango/src/components/SignUp/index.js
@@ -1,31 +1,59 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+
+import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
+import * as ROLES from '../../constants/roles';
+
 const SignUpPage = () => (
     <div>
         <h1>SignUp</h1>
         <SignUpForm />
     </div>
 );
-class SignUpForm extends Component {
+const INITIAL_STATE = {
+    username: '',
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    isAdmin: false,
+    error: null,
+};
+
+class SignUpFormBase extends Component {
     constructor(props) {
         super(props);
         this.state = { ...INITIAL_STATE };
-
     }
     onSubmit = event => {
+        const { username, email, passwordOne, isAdmin } = this.state;
+        const roles = [];
+        if (isAdmin) {
+            roles.push(ROLES.ADMIN);
+        }
+        this.props.firebase
+            .doCreateUserWithEmailAndPassword(email, passwordOne)
+            .then(authUser => {
+                // Create a user in your Firebase realtime database
+                return this.props.firebase
+                    .user(authUser.user.uid)
+                    .set({
+                        username,
+                        email,
+                        roles,
+                    });
+            })
+            .catch(error => {
+                this.setState({ error });
+            });
+        event.preventDefault();
     }
-    onChange = event => {
-    };
-    render() {
-        return (
-            <form onSubmit={this.onSubmit}>
-            </form>
-        );
-    }
-
     onChange = event => {
         this.setState({ [event.target.name]: event.target.value });
+    };
+    onChangeCheckbox = event => {
+        this.setState({ [event.target.name]: event.target.checked });
     };
     render() {
         const {
@@ -33,8 +61,15 @@ class SignUpForm extends Component {
             email,
             passwordOne,
             passwordTwo,
+            isAdmin,
             error,
         } = this.state;
+
+        const isInvalid =
+            passwordOne !== passwordTwo ||
+            passwordOne === '' ||
+            email === '' ||
+            username === '';
 
         return (
             <form onSubmit={this.onSubmit}>
@@ -66,7 +101,19 @@ class SignUpForm extends Component {
                     type="password"
                     placeholder="Confirm Password"
                 />
-                <button type="submit">Sign Up</button>
+                <label>
+                    Admin:
+                    <input
+                        name="isAdmin"
+                        type="checkbox"
+                        checked={isAdmin}
+                        onChange={this.onChangeCheckbox}
+                    />
+                </label>
+                <button disabled={isInvalid} type="submit">
+                    Sign Up
+                </button>
+
                 {error && <p>{error.message}</p>}
             </form>
         );
@@ -78,13 +125,10 @@ const SignUpLink = () => (
     </p>
 );
 
-const INITIAL_STATE = {
-    username: '',
-    email: '',
-    passwordOne: '',
-    passwordTwo: '',
-    error: null,
-};
+const SignUpForm = compose(
+    withRouter,
+    withFirebase,
+)(SignUpFormBase);
 
 export default SignUpPage;
 export { SignUpForm, SignUpLink };
