c809d39ca580d409c9f8b7d664a1233057e6d7aa
TomEder <tomederstal@gmail.com>
2020-02-19 20:53:33 +0100

    Added an API

--- fe19tp2_mango/src/components/Home/index.js
+++ fe19tp2_mango/src/components/Home/index.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import { withAuthorization } from '../Session';
-const HomePage = () => (
-    <div>
-        <h1>Home Page</h1>
-        <p>The Home Page is accessible by every signed in user.</p>
-    </div>
-);
+import axios from 'axios'
+import { Line, Bar } from 'react-chartjs-2';
+import Chart  from '../Chart'
+
+
+class HomePage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            result: null,
+        }
+
+    }
+    componentDidMount() {
+
+        let crimes = {
+            data: ''
+        }
+        axios({
+            method: 'get',
+            url: `https://cors-anywhere.herokuapp.com/https://brottsplatskartan.se/api/events/? limit=100`
+        })
+            .then(result => {
+                console.log(result)
+                this.setState({ result: result.data.data })
+                console.log(this.state.result)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+    render() {
+        return (
+            <div>
+                <h1>Home Page</h1>
+                <p>The Home Page is accessible by every signed in user.</p>
+                <Chart ></Chart>
+            </div>
+
+        )
+    }
+}
 const condition = authUser => !!authUser;
 export default withAuthorization(condition)(HomePage);
--- fe19tp2_mango/src/components/Navigation/index.js
+++ fe19tp2_mango/src/components/Navigation/index.js
@@ -3,35 +3,36 @@ import { Link } from 'react-router-dom';
 import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
 import { AuthUserContext } from '../Session';
+import * as ROLES from '../../constants/roles'
 
 
 const Navigation = () => (
-    <div>
-        <AuthUserContext.Consumer>
-            {authUser =>
-                authUser ? <NavigationAuth /> : <NavigationNonAuth />
-            }
-        </AuthUserContext.Consumer>
-    </div>
+    <AuthUserContext.Consumer>
+        {authUser =>
+            authUser ? (
+                <NavigationAuth authUser={authUser} />
+            ) : (<NavigationNonAuth />
+                )}
+    </AuthUserContext.Consumer>
 );
-const NavigationAuth = () => (
-    <ul>
-        <li>
-            <Link to={ROUTES.LANDING}>Landing</Link>
-        </li>
+
+const NavigationAuth = ({ authUser }) => (
+    <ul> <li>
+        <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
         <li>
             <Link to={ROUTES.HOME}>Home</Link>
-        </li>
-        <li>
+        </li> <li>
             <Link to={ROUTES.ACCOUNT}>Account</Link>
         </li>
-        <li>
-            <Link to={ROUTES.ADMIN}>Admin</Link>
-        </li>
+        {authUser.roles.includes(ROLES.ADMIN) && (
+            <li>
+                <Link to={ROUTES.ADMIN}>Admin</Link>
+            </li>
+        )}
         <li>
             <SignOutButton />
-        </li>
-    </ul>
+        </li> </ul>
 );
 
 const NavigationNonAuth = () => (
